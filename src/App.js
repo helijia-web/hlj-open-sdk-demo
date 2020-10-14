9148d18be2c46910f5425af65eb9f0f3c5be704c
bencode <bencode@163.com>
2020-10-14 16:12:53 +0800

    feat: 添加页面跳转api

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import {
   ajax,
   checkLogin,
   login,
-  trackAction
+  trackPage,
+  trackAction,
+  go,
+  hljurl
 } from 'hlj-open-sdk';
 import 'hlj-open-sdk/dist/lib.css';
 import './App.scss';
@@ -13,14 +16,19 @@ function App() {
 
   useEffect(() => {
     const load = async() => {
+      // 检测登录
       const value = await checkLogin();
       setLogined(value);
 
+      // 访问ajax
       const url = 'https://p.helijia.com/api/magic/v2/pages/clientapp-index';
       const data = await ajax({ url });
       console.log(data);
     }
     load();
+
+    // 进入页面埋点
+    trackPage('b-face-test');
   }, []);
 
   const handleLogin = () => {
@@ -29,6 +37,12 @@ function App() {
 
   const handleTrack = () => {
     trackAction('b-face-test.click');
+  };
+
+  const handleJump = () => {
+    // 由运营使用乐高工具生成
+    const url = 'hljclient://page?jsonData={"type":4,"pageName":"HTML","data":{"url1":"https://m.helijia.com"}}';
+    go(hljurl(url));
   };
 
   return (
@@ -43,6 +57,10 @@ function App() {
       <section>
         <h2>埋点</h2>
         <button onClick={handleTrack}>点我</button>
+      </section>
+      <section>
+        <h2>链接和跳转</h2>
+        <button onClick={handleJump}>跳转页面</button>
       </section>
     </div>
   );
